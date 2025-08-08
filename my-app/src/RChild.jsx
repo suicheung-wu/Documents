3c17d3ade0ce18a313c62d9f4a8749d6297ce65e
suicheung <suicheungwu@gmail.com>
2025-08-08 11:03:07 +0800

    clean unit setting

---
+++ my-app/src/RChild.jsx
@@ -0,0 +1,10 @@
+// import "./App.css";
+import React from "react";
+import RChild from "./RChild";
+function App() {
+    return (
+        <React.Fragment>
+            <RChild key='1' id='testId' content='hello world' />
+            <RChild key='2' id='testId2' content='hello'
+    }
+}
---
+++ my-app/src/RChild1.jsx
@@ -0,0 +1,11 @@
+const RChild = ({ id, content }) => {
+    return (
+        <>
+            <div>
+                <h1>Hello</h1>
+                <p id={id}>{content}
+
+        </div>
+            
+            )
+}
